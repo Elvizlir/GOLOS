b9b0abc09ee77a45029610c435e4cef8f1c1b104
Roman Makeev <elvizlir@gmail.com>
2019-09-09 12:47:40 +0300

    all done agan

--- public/js/common.js
+++ public/js/common.js
@@ -2,10 +2,13 @@ var $ = jQuery;
 
 jQuery(document).ready(function ($) {
 
-  $(".main-wrapper").after('<div class="screen" style="background-image: url(screen/screen.jpg);"></div>')
+    $(".custom-input__input").change(function () {
+      if ($(this).is(":checked")) {
+        $('#btn').removeClass("disabled");
+      }
+    })
 
-});
+  });
 
 // JSCCommon.LazyFunction();
 /***/
-
