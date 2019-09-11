30295556e6a358f36e37382fbf11c837c5313653
Roman Makeev <elvizlir@gmail.com>
2019-09-11 12:25:05 +0300

    edit viewport

--- sourse/js/common.js
+++ sourse/js/common.js
@@ -2,13 +2,13 @@ var $ = jQuery;
 
 jQuery(document).ready(function ($) {
 
-    $(".custom-input__input").change(function () {
-      if ($(this).is(":checked")) {
-        $('#btn').removeClass("disabled");
-      }
-    })
+  $(".custom-input__input").change(function () {
+    if ($(this).is(":checked")) {
+      $('#btn').removeClass("disabled");
+    }
+  })
 
-  });
+});
 
 // JSCCommon.LazyFunction();
 /***/
